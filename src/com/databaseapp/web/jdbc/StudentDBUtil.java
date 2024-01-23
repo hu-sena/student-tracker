ff336c73a1a1b86a02606235d74766e20b2cdaa7
Husna Zubir <shusnazubir@gmail.com>
2024-01-23 08:36:41 +0800

    create StudentDBUtil DAO to database

---
+++ src/com/databaseapp/web/jdbc/StudentDBUtil.java
@@ -0,0 +1,26 @@
+package com.databaseapp.web.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+public class StudentDBUtil {
+	
+	// initialize the source (database)
+	private DataSource dataSource;
+	
+	// dependency injection
+	public StudentDBUtil(DataSource theDataSource) {
+		dataSource = theDataSource;
+	}
+	
+	public List<Student> getStudents() throws Exception {
+		List<Student> students = new ArrayList<>();
+		
+		return students;
+	}
+}
